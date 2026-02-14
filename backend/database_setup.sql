d9767b1f7cd3b08d03f2f0bc85d9772de9874769
Helen751 <okerekehelenugoeze@gmail.com>
2026-02-14 16:18:27 +0200

    Created the database schema and improved data loading performance using chunked inserts

---
+++ backend/database_setup.sql
@@ -0,0 +1,110 @@
+# Create the database for the HIT Group Urban Mobility project
+CREATE DATABASE IF NOT EXISTS HIT_urban_mobility_db 
+CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+# Use the created database
+USE `HIT_urban_mobility_db`;
+
+# Setting the time zone to UTC for consistent timestamp across any devices and locations
+SET TIME_ZONE = '+00:00';
+
+# Setting the SQL mode to enforce strict data integrity and prevent invalid data from being inserted into the tables
+SET sql_mode = 'STRICT_TRANS_TABLES,NO_ENGINE_SUBSTITUTION';
+
+
+# Creating the vendors table
+CREATE TABLE IF NOT EXISTS vendors (
+    vendor_id INT PRIMARY KEY COMMENT 'Storing the vendor ID which can have more entities in the future'
+) ENGINE=InnoDB;
+
+# Creating the boroughs table
+CREATE TABLE IF NOT EXISTS boroughs (
+    borough_id INT AUTO_INCREMENT PRIMARY KEY COMMENT 'ID for the boroughs',
+    borough_name VARCHAR(50) UNIQUE NOT NULL COMMENT 'Storing the unique name of the boroughs in New York City'
+) ENGINE=InnoDB;
+
+# Creating the service zones table
+CREATE TABLE IF NOT EXISTS service_zones (
+    service_zone_id INT AUTO_INCREMENT PRIMARY KEY COMMENT 'ID for the service zones',
+    service_zone_name VARCHAR(50) UNIQUE NOT NULL COMMENT 'Storing the unique name of the service zones in New York City'
+) ENGINE=InnoDB;
+
+CREATE TABLE IF NOT EXISTS locations (
+    location_id INT PRIMARY KEY COMMENT 'Pick up LocationID / Drop off LocationID',
+    zone_name VARCHAR(100) NOT NULL COMMENT 'Storing the name of the zone for the location',
+    borough_id INT NOT NULL COMMENT 'Storing the borough ID for the location',
+    service_zone_id INT NOT NULL COMMENT 'Storing the service zone ID for the location',
+
+    -- Creating the foreign keys and Adding the ON DELETE and ON UPDATE actions to prevent deletes and update rule
+    FOREIGN KEY (borough_id) REFERENCES boroughs(borough_id) ON DELETE RESTRICT ON UPDATE CASCADE,
+    FOREIGN KEY (service_zone_id) REFERENCES service_zones(service_zone_id) ON DELETE RESTRICT ON UPDATE CASCADE,
+
+    -- Creating the borough index to optimize the joins
+    INDEX idx_locations_borough (borough_id),
+    INDEX idx_locations_service_zone (service_zone_id)
+) ENGINE=InnoDB;
+
+# Creating the payment types table
+CREATE TABLE IF NOT EXISTS payment_types (
+    payment_type_id INT PRIMARY KEY COMMENT 'ID for the payment types',
+    description VARCHAR(50) COMMENT 'Storing the Payment type eg CARD'
+) ENGINE=InnoDB;
+
+# Stroing the rate codes
+CREATE TABLE IF NOT EXISTS rate_codes (
+    rate_code_id INT PRIMARY KEY COMMENT 'ID for the rate codes',
+    description VARCHAR(100) COMMENT 'Storing the description of the rate code'
+) ENGINE=InnoDB;
+
+# Creating the trips table to link all the records together and store the trip data
+CREATE TABLE IF NOT EXISTS trips (
+    trip_id BIGINT AUTO_INCREMENT PRIMARY KEY COMMENT 'ID for each trip',
+
+    vendor_id INT NOT NULL COMMENT 'Storing the vendor ID for the trip',
+    pickup_datetime DATETIME NOT NULL COMMENT 'Storing the pickup date and time',
+    dropoff_datetime DATETIME NOT NULL COMMENT 'Storing the dropoff date and time',
+
+    passenger_count INT NOT NULL COMMENT 'Storing the number of passengers for the trip',
+    trip_distance DECIMAL(6,2) NOT NULL COMMENT 'Storing the distance of the trip in miles',
+
+    rate_code_id INT NOT NULL COMMENT 'Storing the rate code ID for the trip',
+    store_and_fwd_flag CHAR(1) NOT NULL CHECK (store_and_fwd_flag IN ('Y','N')) COMMENT 'Storing the store and forward flag for the trip',
+
+    pickup_location_id INT NOT NULL COMMENT 'Storing the pickup location ID for the trip',
+    dropoff_location_id INT NOT NULL COMMENT 'Storing the dropoff location ID for the trip',
+
+    payment_type_id INT NOT NULL COMMENT 'Storing the payment type ID for the trip',
+
+    -- Storing the financial details of the trip
+    fare_amount DECIMAL(8,2) NOT NULL,
+    extra DECIMAL(6,2) NOT NULL DEFAULT 0,
+    mta_tax DECIMAL(6,2) NOT NULL DEFAULT 0,
+    tip_amount DECIMAL(8,2) NOT NULL DEFAULT 0,
+    tolls_amount DECIMAL(8,2) NOT NULL DEFAULT 0,
+    improvement_surcharge DECIMAL(6,2) NOT NULL DEFAULT 0,
+    total_amount DECIMAL(8,2) NOT NULL,
+
+    trip_duration_min DECIMAL(6,2) NOT NULL COMMENT 'Storing the duration of the trip in minutes',
+    avg_speed_mph DOUBLE NOT NULL COMMENT 'Storing the average speed of the trip in miles per hour',
+    fare_per_mile DECIMAL(6,2) NOT NULL COMMENT 'Storing the fare per mile for the trip',
+
+    -- Creating a unique trip
+    UNIQUE KEY unique_trip (vendor_id, pickup_datetime, dropoff_datetime, pickup_location_id, dropoff_location_id),
+
+    -- Creating the foreign keys and Adding the ON DELETE and ON UPDATE actions to prevent deletes and update rule
+    FOREIGN KEY (vendor_id) REFERENCES vendors(vendor_id) ON DELETE RESTRICT ON UPDATE CASCADE,
+    FOREIGN KEY (rate_code_id) REFERENCES rate_codes(rate_code_id) ON DELETE RESTRICT ON UPDATE CASCADE,
+    FOREIGN KEY (payment_type_id) REFERENCES payment_types(payment_type_id) ON DELETE RESTRICT ON UPDATE CASCADE,
+    FOREIGN KEY (pickup_location_id) REFERENCES locations(location_id) ON DELETE RESTRICT ON UPDATE CASCADE,
+    FOREIGN KEY (dropoff_location_id) REFERENCES locations(location_id) ON DELETE RESTRICT ON UPDATE CASCADE,
+
+    -- Creating all the necessary indexes to optimize the queries on the trips table
+    INDEX idx_trips_pickup_datetime (pickup_datetime),
+    INDEX idx_trips_dropoff_datetime (dropoff_datetime),
+    INDEX idx_trips_vendor (vendor_id),
+    INDEX idx_trips_payment_type (payment_type_id),
+    INDEX idx_trips_rate_code (rate_code_id),
+    INDEX idx_trips_pickup_location (pickup_location_id),
+    INDEX idx_trips_dropoff_location (dropoff_location_id)
+) ENGINE=InnoDB;
+
---
+++ backend/load_data_to_sql.py
@@ -0,0 +1,241 @@
+import os
+import sys
+import pandas as pd
+import mysql.connector
+import time
+
+# load the cleaned CSV file and check if it exists
+base_dir = os.path.dirname(os.path.abspath(__file__))
+file_path = os.path.join(base_dir, "processed", "cleaned_trips.csv")
+
+if not os.path.exists(file_path):
+    print("Error: cleaned_trips.csv not found. Run the pipeline.py first to generate the cleaned CSV file.\n")
+    sys.exit(1)
+
+print("CSV file found. Loading data...")
+
+# Database connection enclosed in try and catch to handle potential connection errors
+try:
+    conn = mysql.connector.connect(
+        host="localhost",
+        user="root",
+        password="",
+        database="HIT_urban_mobility_db" # our db name
+    )
+    cursor = conn.cursor()
+except mysql.connector.Error as err:
+    print("Database connection failed:", err)
+    sys.exit(1)
+
+print("Database connection successful. Time started...\n")
+
+overall_start = time.time()
+# Reading the CSV file into a pandas DataFrame
+
+print("The data file is too large, we will load in chunks to optimize your computer memory")
+print("Now loading CSV data in chunks...\n")
+
+chunk_size = 10000
+vendors = set()
+payment_types = set()
+rate_codes = set()
+boroughs = set()
+zones = set()
+
+# collecting unique locations first (in memory)
+unique_locations = {}
+
+for chunk in pd.read_csv(file_path, chunksize=chunk_size):
+
+    # collecting the unique vendor IDs, payment types, rate codes, boroughs, and zones from the chunk to insert into relational tables
+    vendors.update(chunk["VendorID"].unique())
+    payment_types.update(chunk["payment_type"].unique())
+    rate_codes.update(chunk["RatecodeID"].unique())
+
+    boroughs.update(chunk["PU_Borough"].unique())
+    boroughs.update(chunk["DO_Borough"].unique())
+
+    zones.update(chunk["PU_ServiceZone"].unique())
+    zones.update(chunk["DO_ServiceZone"].unique())
+
+    # Collecting unique locations from the chunk and storing them in a dictionary
+    for row in chunk.itertuples(index=False):
+
+        unique_locations[row.PULocationID] = (
+            row.PU_Zone,
+            row.PU_Borough,
+            row.PU_ServiceZone
+        )
+
+        unique_locations[row.DOLocationID] = (
+            row.DO_Zone,
+            row.DO_Borough,
+            row.DO_ServiceZone
+        )
+print(f"Scanning CSV in chunks completed under {time.time() - overall_start:.2f} seconds. Starting insert...\n")
+
+# building descriptions for rate codes, since it is not given in the CSV
+rate_mapping = {
+    1: "Standard rate",
+    2: "JFK airport flat fare",
+    3: "Newark airport flat fare",
+    4: "Nassau or Westchester fare",
+    5: "Negotiated fare",
+    6: "Group ride"
+}
+
+# building descriptions for payment types, since it is not given in the CSV
+payment_mapping = {
+    1: "Credit card",
+    2: "Cash",
+    3: "No charge",
+    4: "Dispute",
+    5: "Unknown",
+    6: "Voided trip"
+}
+
+# Handling unexpected ID values that are not in the mapping (if any)
+for pt in payment_types:
+    if pt not in payment_mapping:
+        payment_mapping[pt] = "Other"
+
+for rate in rate_codes:
+    if rate not in rate_mapping:
+        rate_mapping[rate] = "Other"
+
+
+# ==== Starting the insertion process ====
+start_time = time.time()
+# Inserting to vendors table first
+vendor_values = [(int(v),) for v in vendors]
+cursor.executemany(
+    "INSERT IGNORE INTO vendors (vendor_id) VALUES (%s)",
+    vendor_values
+)
+
+# Inserting to payment_types table
+payment_type_values = [(int(pt), payment_mapping[pt]) for pt in payment_types]
+cursor.executemany(
+    "INSERT IGNORE INTO payment_types (payment_type_id, description) VALUES (%s, %s)",
+    payment_type_values
+)
+
+# Inserting to rate_codes table
+rate_code_values = [(int(rate), rate_mapping[rate]) for rate in rate_codes]
+cursor.executemany(
+    "INSERT IGNORE INTO rate_codes (rate_code_id, description) VALUES (%s, %s)",
+    rate_code_values
+)
+
+borough_values = [(borough,) for borough in boroughs]
+cursor.executemany(
+    "INSERT IGNORE INTO boroughs (borough_name) VALUES (%s)",
+    borough_values
+)
+
+zone_values = [(zone,) for zone in zones]
+cursor.executemany(
+    "INSERT IGNORE INTO service_zones (service_zone_name) VALUES (%s)",
+    zone_values
+)
+# running the commit first, before inserting into trips and locations for foreign key dependencies
+conn.commit()
+
+print(f"Relational tables populated under {time.time() - start_time:.2f} seconds. Building locations...\n")
+
+start_time = time.time()
+
+# building a dictionary to map location and optimize for O(1) lookups
+cursor.execute("SELECT borough_id, borough_name FROM boroughs")
+borough_map = {name: id for id, name in cursor.fetchall()}
+
+cursor.execute("SELECT service_zone_id, service_zone_name FROM service_zones")
+zone_map = {name: id for id, name in cursor.fetchall()}
+
+location_values = []
+
+# Inserting locations at once to optimize for speed
+for location_id, (zone_name, borough_name, service_zone_name) in unique_locations.items():
+
+    location_values.append((
+        location_id,
+        zone_name,
+        borough_map[borough_name],
+        zone_map[service_zone_name]
+    ))
+
+cursor.executemany(
+    "INSERT IGNORE INTO locations (location_id, zone_name, borough_id, service_zone_id) VALUES (%s, %s, %s, %s)",
+    location_values
+)
+
+conn.commit()
+
+print(f"All locations inserted in {time.time() - start_time:.2f} seconds. Now inserting trips...\n")
+del unique_locations # clearing the unique_locations dictionary from memory since it is no longer needed
+
+# Starting the timer for trip insertion
+start_time = time.time()
+chunk_size = 10000
+rows_inserted = 0
+chunk_counter = 0
+commit_every = 5 # commit after every 5 chunks to manage memory and speed up inserting
+
+# Inserting into trips table with all foreign keys already in place
+query = "INSERT INTO trips (vendor_id, pickup_datetime, dropoff_datetime, passenger_count, trip_distance, rate_code_id, store_and_fwd_flag, pickup_location_id, dropoff_location_id, payment_type_id, fare_amount, extra, mta_tax, tip_amount, tolls_amount, improvement_surcharge, total_amount, trip_duration_min, avg_speed_mph, fare_per_mile) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)"
+
+
+# iterating through the DataFrame and inserting each trip record into the trips table in chunks
+
+for chunk in pd.read_csv(file_path, chunksize=chunk_size): 
+
+    trip_values = []
+
+    for each_trip in chunk.itertuples(index=False):
+        trip_values.append((
+            each_trip.VendorID,
+            each_trip.tpep_pickup_datetime,
+            each_trip.tpep_dropoff_datetime,
+            each_trip.passenger_count,
+            each_trip.trip_distance,
+            each_trip.RatecodeID,
+            each_trip.store_and_fwd_flag,
+            each_trip.PULocationID,
+            each_trip.DOLocationID,
+            each_trip.payment_type,
+            each_trip.fare_amount,
+            each_trip.extra,
+            each_trip.mta_tax,
+            each_trip.tip_amount,
+            each_trip.tolls_amount,
+            each_trip.improvement_surcharge,
+            each_trip.total_amount,
+            each_trip.trip_duration_min,
+            each_trip.avg_speed_mph,
+            each_trip.fare_per_mile
+        ))
+
+    cursor.executemany(query, trip_values)
+    
+    chunk_counter += 1
+    if chunk_counter % commit_every == 0:
+        conn.commit()
+
+    rows_inserted += len(trip_values)
+    print(f"Inserted {rows_inserted} trips so far...")
+
+conn.commit() # final commit for any remaining records
+
+print("Trips inserted successfully, counting the number of records...\n")
+trip_time = time.time() - start_time
+
+cursor.execute("SELECT COUNT(*) FROM trips")
+trip_count = cursor.fetchone()[0]
+
+print("Data successfully loaded into MySQL.\n\n")
+print(f"\tTotal number of trips inserted: {trip_count}")
+print("\tTime used for Trip insertion:", round(trip_time, 2), "seconds")
+print("\tTotal Time Used:", round(time.time() - overall_start, 2), "seconds\n\n")
+
+cursor.close()
+conn.close() 
